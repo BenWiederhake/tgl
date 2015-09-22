745ccab63c5d6465e1aa9f05271883e89114cc33
Ben Wiederhake <BenWiederhake.GitHub@gmx>
2015-09-22 13:10:48 +0200

    Include license ref in each header.

--- crypto/importer.sh
+++ crypto/importer.sh
@@ -6,6 +6,26 @@ for dst in aes bn err hmac md5 pem rand rsa sha
 do
   cpptag=__CRYPTO_`echo $dst | tr '[a-z]' '[A-Z]'`_H__
   cat <<MYEOF > $dst.h
+/* 
+    This file is part of tgl-library
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright Vitaly Valtman 2013-2015
+*/
+
 #ifndef $cpptag
 #define $cpptag
 
